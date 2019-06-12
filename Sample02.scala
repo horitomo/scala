1ab0bb812f784c32a80cd6d28f2c744af504b4f4
horitomo <ths75149@gmail.com>
2019-06-12 14:13:07 +0900

    [ADD] コメント

---
+++ Sample02.scala
@@ -0,0 +1,10 @@
+// 2019年6月12日作成
+object Sample02{
+                    def main(args:Array[String]){
+                                        println("こんにちは")
+                                        /*
+                                        println("こんばんは")
+                                        println("さようなら")
+                                        */
+                    }
+}

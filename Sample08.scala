9ba0ed2f3793ede6b088a739263505e429cf6291
horitomo <ths75149@gmail.com>
2019-06-13 16:01:12 +0900

    [ADD] 変数

---
+++ Sample08.scala
@@ -0,0 +1,13 @@
+object Sample08{
+                    def main(args:Array[String]){
+                                        val x1:Int = 10
+                                        // x1 = 20
+                                        var x2:Int = 30
+                                        x2 = 40
+                                        val x3:Double = x2
+                                        // val x4:Int = "Hello"
+                                        println(x1)
+                                        println(x2)
+                                        println(x3)
+                    }
+}

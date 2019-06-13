89cfd7c451d95e301c856686b516f0e74eef87b5
horitomo <ths75149@gmail.com>
2019-06-13 15:55:58 +0900

    [ADD] 文字列型

---
+++ Sample07.scala
@@ -0,0 +1,8 @@
+object Sample07{
+                    def main(args:Array[String]){
+                                        println("おはよう")
+                                        println("こんにちは\nこんばんは")
+                                        println("""'さようなら'
+                                        "おやすみ"""")
+                    }
+}

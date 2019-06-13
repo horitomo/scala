16c4327a6229fd81f4e8f97cf8ce25490682e665
horitomo <ths75149@gmail.com>
2019-06-13 15:50:33 +0900

    [ADD] 文字型

---
+++ Sample06.scala
@@ -0,0 +1,11 @@
+object Sample06{
+                    def main(args:Array[String]){
+                                        println('A')
+                                        println('あ')
+                                        println('亜')
+                                        println('\'')
+                                        println('\\')
+                                        println('\u0041')
+                                        // println('ABC")
+                    }
+}

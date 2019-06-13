675a2184b307f948d89a8d16787225d49f6d0a97
horitomo <ths75149@gmail.com>
2019-06-13 14:17:51 +0900

    [ADD] 整数型

---
+++ Sample03.scala
@@ -0,0 +1,8 @@
+object Sample03{
+                    def main(args:Array[String]){
+                                        println(123)
+                                        println(0x78)
+                                        // println(30000000000) コンパイルエラー
+                                        println(30000000000L)
+                    }
+}

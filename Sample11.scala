858da9d9ceb121d8e7a7c8df43e7559e0e59c93a
horitomo <ths75149@gmail.com>
2019-06-13 16:21:47 +0900

    [ADD] 関係演算子

---
+++ Sample11.scala
@@ -0,0 +1,11 @@
+object Sample11{
+                    def main(args:Array[String]){
+                                        val x = 7
+                                        val y = 3
+                                        println( x + "==" + y + ":" + ( x == y ))
+                                        println( x + "!=" + y + ":" + ( x != y ))
+                                        println( x + ">" + y + ":" + ( x > y ))
+                                        println( x + "<" + y + ":" + ( x < y ))
+
+                    }
+}

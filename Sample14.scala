0978303275d20c2a211d5863674eb7e2fa16e5aa
horitomo <ths75149@gmail.com>
2019-06-19 14:57:50 +0900

    [ADD] if式の基本

---
+++ Sample14.scala
@@ -0,0 +1,13 @@
+object Sample14{
+                    def main(args:Array[String]){
+                                        val age = 18
+                                        println(age + "歳は")
+                                        if(age >= 20){
+                                                            println("成人です")
+                                        }else if(age < 20 && age >= 0){
+                                                            println("未成年です")
+                                        }else {
+                                                            println("不明です")
+                                        }
+                    }
+}

e48dc23ae93194faca654ffe107fa9ce9e57b214
horitomo <ths75149@gmail.com>
2019-06-20 18:23:00 +0900

    [UPD] while変更

---
+++ Practice03.scala
@@ -0,0 +1,13 @@
+object Practice03{
+                    def main(args:Array[String]){
+                                        // 答え変数（１から１０までの値をランダムに設定）
+                                        val answer = scala.util.Random.nextInt(10)+1
+
+                                        var count = 3
+                                        while(count = 0){
+                                                            
+                                                            println("数値を入力してください（残り"+count+"回")
+                                                            count -=1
+                                        }
+                    }
+}

876b2641f3780a717d868e53ef306928427872d1
horitomo <ths75149@gmail.com>
2019-06-20 18:19:28 +0900

    [ADD] while,do-while

---
+++ Sample16.scala
@@ -0,0 +1,17 @@
+object Sample16{
+                    def main(args:Array[String]){
+                                        println("while ループ")
+                                        var i = 0
+                                        while(i < 5){
+                                                            println(i)
+                                                            i += 1
+                                        }
+
+                                        println("do-while ループ")
+                                        var j = 10
+                                        do{
+                                                            println(j)
+                                                            j += 1
+                                        }while(j<5)
+                    }
+}

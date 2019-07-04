18a306e48a5007fdf21dfdec8e978272fee19bf3
horitomo <ths75149@gmail.com>
2019-07-04 16:03:31 +0900

    [ADD] オーバーロード

---
+++ Sample18.scala
@@ -0,0 +1,22 @@
+object Sample18{
+                    def main(args:Array[String]){
+                                        val a = 3;
+                                        val b = 5
+                                        val c = 1.2
+                                        sum(a,b)
+                                        sum(b,c)
+                                        sum(c,a)
+                    }
+
+                    def sum(x:Int, y:Int){
+                                        println("Int + Int =>"+(x+y))
+                    }
+
+                    def sum(x:Int, y:Double){
+                                        println("Int + Double =>"+(x+y))
+                    }
+
+                    def sum(x:Double, y:Int){
+                                        println("Double + Int =>"+(x+y))
+                    }
+}
